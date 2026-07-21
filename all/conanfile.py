2628a37fa6060806722b7f00b512ff74e9ad9fea
Khalil Estell <kammce0@gmail.com>
2026-07-21 06:49:44 -0700

    :sparkles: Add LLVM 22

--- all/conanfile.py
+++ all/conanfile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2024 - 2025 Khalil Estell and the libhal contributors
+# Copyright 2026 Khalil Estell and the libhal contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
